d931ba953d4e0df88ab35f1a88e221769ce2e7c6
FTC 7 <techbrick7@gmail.com>
2016-11-29 21:01:44 -0500

    --Commented (currently) unused code; going to have to be uncommented when anything is used --Moved stuff around to fix the scope of variables to a limited as possible --Added a function to drive to a vuforia target --Added a function to convert from vuforia units to inches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
@@ -5,8 +5,8 @@ package org.firstinspires.ftc.teamcode;
         10in:225
         in ≈ 22.5 units
  */
-import android.app.Activity;
-import android.view.View;
+/*import android.app.Activity;
+import android.view.View;*/
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -23,37 +23,43 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefau
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 import static java.lang.Math.round;
-import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES
-        ;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
+
 @Autonomous(name="AlphaAutonomous", group="Autonomous")
 public class autonomous extends LinearOpMode {
 
-    ColorSensor csensor;
-
-    hopefullymeccanum robot = new hopefullymeccanum();
+    private hopefullymeccanum robot = new hopefullymeccanum();
+    private VuforiaLocalizer vuforia;
+    private VuforiaTrackables beacons;
 
     //robot.init(hardwareMap);
-
-    public void runOpMode() throws InterruptedException {
-        float hsvValues[] = {0F,0F,0F};
-        final float values[] = hsvValues;
-        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
-        csensor = hardwareMap.colorSensor.get("colorSense");
-        csensor.enableLed(false);
-
+    public autonomous() {
+        super();
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         params.vuforiaLicenseKey = "AaIK0f//////AAAAGTsgmHszM030skFBvcnAJlNSWaH7oKLZhxAZeCi7ToBGSKkO7T3EvzsRVYQdyDp2X+TFK6TQs+3WoCHkZXDYPQd87f77D6kvcBr8zbJ07Fb31UKiXdUBvX+ZQSV3kBhdAoxhfMa0WPgys7DYaeiOmM49CsNra7nVh05ls0th3h07wwHz3s/PBZnQwpbfr260CDgqBv4e9D79Wg5Ja5p+HAOJvyqg2r/Z5dOyRvVI3f/jPBRZHvDgDF9KTcuJAPoDHxfewmGFOFtiUamRLvcrkK9rw2Vygi7w23HYlzFO7yap+jUk1bv0uWNc0j5HPJDAjqa2ijBN9aVDrxzmFJml5WMA3GJJp8WOd9gkGhtI/BIo";
         params.cameraMonitorFeedback = AXES;
 
-        VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(params);
-        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+        vuforia = ClassFactory.createVuforiaLocalizer(params);
 
-        VuforiaTrackables beacons = vuforia.loadTrackablesFromAsset("FTC_2016-17");
+        beacons = vuforia.loadTrackablesFromAsset("FTC_2016-17");
         beacons.get(0).setName("Wheels");
         beacons.get(1).setName("Tools");
         beacons.get(2).setName("Legos");
         beacons.get(3).setName("Gears");
+    }
+
+    public void runOpMode() throws InterruptedException {
+        ColorSensor csensor;
+
+        //float hsvValues[] = {0F,0F,0F};
+        //final float values[] = hsvValues;
+        //final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
+        csensor = hardwareMap.colorSensor.get("colorSense");
+        csensor.enableLed(false);
+
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+
 
         robot.init(hardwareMap);
 
@@ -96,6 +102,8 @@ public class autonomous extends LinearOpMode {
         robot.backRight.setPower(0);
 
 
+        move(0, 12, 1);
+
         /*while(opModeIsActive()){
             Color.RGBToHSV(csensor.red(), csensor.green(), csensor.blue(), hsvValues);
             telemetry.addData("Hue", hsvValues[0]);
@@ -116,7 +124,39 @@ public class autonomous extends LinearOpMode {
         robot.backRight.setPower(0.1);*/
     }
 
-    void move(short direction, long distance, long speed) throws InterruptedException{
+    /*int driveToVuforia(int direction, long xpos, long speed){
+        startMove(direction, speed);
+
+        boolean running = true;
+
+        while (running){
+            telemetry.addData("Camera", vuforia.getCameraCalibration());
+
+            for(VuforiaTrackable beac : beacons){
+                OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) beac.getListener()).getPose();
+
+                if(pose != null){
+                    VectorF translation = pose.getTranslation();
+
+                    telemetry.addData(beac.getName() + "-Translation", translation);
+
+                    telemetry.addData(beac.getName() + "-X:", round(translation.get(0)));       //Positive is when the target is higher than the phone
+                    telemetry.addData(beac.getName() + "-Y:", round(translation.get(1)));       //Positive is when the target is righter than the phone
+                    telemetry.addData(beac.getName() + "-Z:", round(translation.get(2)));       //NEGATIVE is when the target is behind than the phone (visible with the back camera)
+                    if (translation.get(1) < xpos){
+                        running = false;
+                    }
+                }
+            }
+            telemetry.update();
+        }
+
+        stopMove();
+
+        return 0;
+    }*/
+
+    private void move(int direction, long distance, long speed) throws InterruptedException{
         speed /= 100;
 
         startMove(direction, speed);
@@ -126,7 +166,7 @@ public class autonomous extends LinearOpMode {
         stopMove();
     }
 
-    void startMove(short direction, long speed) {
+    private void startMove(int direction, long speed) {
 
         switch (direction) {
             case (0): {             //Forwards
@@ -156,11 +196,15 @@ public class autonomous extends LinearOpMode {
         }
     }
 
-    void stopMove() {
+    private void stopMove() {
         robot.frontLeft.setPower(0);
         robot.frontRight.setPower(0);
         robot.backLeft.setPower(0);
         robot.backRight.setPower(0);
     }
+
+    /*long vuforiaInches(double vuforiaIn) {
+        return (long) (vuforiaIn / 22.5);
+    }*/
 
 }
