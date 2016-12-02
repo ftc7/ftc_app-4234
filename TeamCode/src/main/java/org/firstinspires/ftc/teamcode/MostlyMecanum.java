d2fc675217ec5bd1769a367920bb9fb21153975f
FTC 7 <techbrick7@gmail.com>
2016-12-01 20:18:45 -0500

    --Cleaned up MostlyMecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MostlyMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MostlyMecanum.java
@@ -7,8 +7,6 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name="MostlyMecanum", group="Mecanum")
 public class MostlyMecanum extends OpMode{
 
-    private double Ch1,Ch3,Ch4,FrontLeft,BackLeft,FrontRight,BackRight;
-
     /* Declare OpMode members. */
     private hopefullymeccanum robot = new hopefullymeccanum(); // use the class created to define a Mecanobot's hardware
 
@@ -46,18 +44,9 @@ public class MostlyMecanum extends OpMode{
      */
     @Override
     public void loop() {
-        /*double left;
-        double right;
+        double Ch1,Ch3,Ch4,FrontLeft,BackLeft,FrontRight,BackRight;
 
-        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
-        left = -gamepad1.left_stick_y;
-        right = -gamepad1.right_stick_y;
-        robot.frontLeft.setPower(-left);
-        robot.frontRight.setPower(right);
-        robot.backLeft.setPower(-left);
-        robot.backRight.setPower(right);*/
-
-        //BETA mecanum drive
+        //Mecanum drive
         Ch1 = gamepad1.right_stick_x;
         Ch3 = -gamepad1.left_stick_y;
         Ch4 = gamepad1.left_stick_x;
@@ -71,10 +60,6 @@ public class MostlyMecanum extends OpMode{
         robot.frontRight.setPower(Range.clip(si(FrontRight, 1.5), -1, 1));
         robot.backLeft.setPower(Range.clip(si(BackLeft, 1.5), -1, 1));
         robot.backRight.setPower(Range.clip(si(BackRight, 1.5), -1, 1));
-
-        // Send telemetry message to signify robot running;
-        /*telemetry.addData("left",  "%.2f", left);
-        telemetry.addData("right", "%.2f", right);*/
 
         telemetry.addData("frontRight", FrontRight);
         telemetry.addData("frontLeft", FrontLeft);
@@ -91,7 +76,7 @@ public class MostlyMecanum extends OpMode{
     public void stop() {
     }
 
-    double si(double joyVal, double divBy) {
+    private double si(double joyVal, double divBy) {
         /**
          * This function scales the input.
          * It makes it so that when you push the joystick forward, the robot doesn't respond in a linear way.
