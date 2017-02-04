88c23c043f47c111b2c29f8e0b3934727b80b7b9
FTC 7 <techbrick7@gmail.com>
2017-02-04 14:58:24 -0500

    Autonomous to prove it's a hardware issue
     - Drives what should be sideways

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sideways.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="SimplySideways", group="Drive")
+public class sideways extends LinearOpMode {
+
+    private hopefullymeccanum robot = new hopefullymeccanum();
+
+    public sideways() {       //Called upon invocation of the class
+        super();
+    }
+
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        waitForStart();
+
+        robot.frontLeft.setPower(-1);
+        robot.frontRight.setPower(1);
+        robot.backLeft.setPower(1);
+        robot.backRight.setPower(-1);
+
+        Thread.sleep(30000);
+
+        robot.frontLeft.setPower(0);
+        robot.frontRight.setPower(0);
+        robot.backLeft.setPower(0);
+        robot.backRight.setPower(0);
+
+
+        telemetry.addData("Autonomous", "complete.");
+        telemetry.update();
+    }
+}
